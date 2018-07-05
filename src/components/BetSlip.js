6802ca2dd133dfc1bdf50a7ba3f81363658cb7c9
martinburdon <martin@martinburdon.co.uk>
2018-07-05 09:11:51 +0100

    Tidy

--- src/components/BetSlip.js
+++ src/components/BetSlip.js
@@ -14,16 +14,20 @@ class BetSlip extends Component {
     ));
   }
 
+  getTotalStake() {
+    const total = this.props.betSlip.reduce((acc, current) => acc + parseFloat(current.stake || 0), 0).toFixed(2);
+    return parseFloat(total).toFixed(2)
+  }
+
   render() {
     const { betSlip, clearBetSlip, placeBet } = this.props;
-    const totalStake = betSlip.reduce((acc, current) => acc + parseFloat(current.stake || 0), 0).toFixed(2);
     const betList = betSlip.length ? this.getMarketsList() : 'No bets placed';
     return (
       <bet-slip>
         <div className="bet-list">{betList}</div>
         <button type="submit" disabled={!betSlip.length} onClick={placeBet}>Submit bets</button>
         <button type="submit" disabled={!betSlip.length} onClick={clearBetSlip}>Remove all selections</button>
-        <p>Total stake: {parseFloat(totalStake).toFixed(2)}</p>
+        <p>Total stake: {this.getTotalStake()}</p>
       </bet-slip>
     );
   }
